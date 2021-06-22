fa4b0f146c85fc2d79b26bab49437a0729cb2dac
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-06-22 16:51:50 -0300

    creating entities, repositories and mcontrollers

--- valoriza/src/server.ts
+++ valoriza/src/server.ts
@@ -1,9 +1,12 @@
-import express, { response } from 'express'
+import 'reflect-metadata'
+import express from 'express'
+import { router } from './routes'
+
+import './database'
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send('ok baby')
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333);
