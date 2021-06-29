91c4e4ec448128b4e0a1d07e8d966b388f0e1345
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-06-29 19:39:32 -0300

    valoriza - finished

--- valoriza/src/controllers/CreateComplimentController.ts
+++ valoriza/src/controllers/CreateComplimentController.ts
@@ -3,12 +3,13 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class CreateComplimentController {
   async handle(request: Request, response: Response) {
-    const { message, tag_id, user_receiver, user_sender } = request.body
-    
+    const { message, tag_id, user_receiver, } = request.body
+    const { user_id } = request;
+
     const createComplimentService = new CreateComplimentService()
 
     const compliment = await createComplimentService.execute({
-      message, tag_id, user_receiver, user_sender
+      message, tag_id, user_receiver, user_sender: user_id
     })
 
     return response.json(compliment)
--- valoriza/src/routes.ts
+++ valoriza/src/routes.ts
@@ -4,6 +4,11 @@ import { CreateComplimentController } from "./controllers/CreateComplimentContro
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 
 const router = Router()
 
@@ -11,10 +16,20 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserSendComplimentsController = new ListUserSendComplimentsController()
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
+const listTagsController = new ListTagsController()
+const listUsersController = new ListUsersController()
 
 router.post("/users", createUserController.handle)
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliments", createComplimentController.handle)
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
+router.get("/tags", listTagsController.handle)
+
+router.get("/users", ensureAuthenticated, listUsersController.handle)
 
 export { router }
